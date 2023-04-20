86a933305236d7cfb40086591ecd25539edb7abf
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-04-20 12:46:15 +0200

    update app

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
         <div style="text-align: center;">
-            <h1>Vue SSR Application</h1>
-            <p>a dummy application that renders on the server and rehydrates on the client.</p>
+            <h1>Vue SSG Application</h1>
+            <p>a dummy application that is pre rendered and stored on the server (generated) and rehydrates on the client.</p>
             <p>Counter: {{ count }}</p>
             <button @click="count++">Increase</button>
             <button @click="count--">Decrease</button>
