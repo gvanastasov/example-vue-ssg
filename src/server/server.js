14088b6f32346085217a4e44953ccc062093b5c8
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-04-20 12:46:23 +0200

    update server

--- src/server/server.js
+++ src/server/server.js
@@ -2,23 +2,14 @@ import express from 'express';
 import path from 'path'
 import { fileURLToPath } from 'url';
 
-import { renderToString } from 'vue/server-renderer';
-import { createApp } from '../client/app.js'
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = express();
 const port = process.env.PORT || 8080;
 
-server.set('view engine', 'ejs');
-
 server.get('/', (_req, res) => {
-  const app = createApp();
-
-  renderToString(app).then((html) => {
-    res.render(path.join(__dirname, './index.ejs'), { html })
-  });
+  res.sendFile(path.join(__dirname, '../../dist/public/index.html'));
 });
 
 server.use(express.static(path.join(__dirname,'../../dist/public')));
